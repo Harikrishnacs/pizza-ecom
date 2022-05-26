fed29b1bd1954f08a1cf585f62fb9c0215f31ddb
harikrishna <harikrishna6525@gmail.com>
2022-05-26 19:26:23 +0530

    Seeder.js file

--- server/seeder.js
+++ server/seeder.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
-import Product from './models/productModel.s'
+import Product from './models/productModel.js'
 import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
